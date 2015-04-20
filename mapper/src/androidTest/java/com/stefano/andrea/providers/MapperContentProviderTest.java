e8b1a99ce1418d4bf2344ce077ee5034771d1518
stefanogianelli <stefano.gianelli@gmail.com>
2015-04-20 13:19:30 +0200

    Aggiunti test

--- mapper/src/androidTest/java/com/stefano/andrea/providers/MapperContentProviderTest.java
+++ mapper/src/androidTest/java/com/stefano/andrea/providers/MapperContentProviderTest.java
@@ -12,7 +12,9 @@ import android.test.mock.MockContentResolver;
  */
 public class MapperContentProviderTest extends ProviderTestCase2<MapperContentProvider> {
 
-    private static final String NOME = "test";
+    private static final String VIAGGIO = "test";
+    private final static String CITTA = "Roma";
+    private final static String NAZIONE = "Italia";
 
     private MockContentResolver mResolver;
 
@@ -24,15 +26,27 @@ public class MapperContentProviderTest extends ProviderTestCase2<MapperContentPr
     public void setUp() throws Exception {
         super.setUp();
         mResolver = getMockContentResolver();
+        //Aggiungo viaggio di default
         ContentValues values = new ContentValues();
-        values.put(MapperContract.Viaggio.NOME, NOME);
-        Uri uri = mResolver.insert(MapperContract.Viaggio.CONTENT_URI, values);
+        values.put(MapperContract.Viaggio.NOME, VIAGGIO);
+        mResolver.insert(MapperContract.Viaggio.CONTENT_URI, values);
+        //Aggiungo dati citta di default
+        values.clear();
+        values.put(MapperContract.DatiCitta.NOME, CITTA);
+        values.put(MapperContract.DatiCitta.NAZIONE, NAZIONE);
+        values.put(MapperContract.DatiCitta.LATITUDINE, 0);
+        values.put(MapperContract.DatiCitta.LONGITUDINE, 0);
+        mResolver.insert(MapperContract.DatiCitta.CONTENT_URI, values);
     }
 
     @Override
     public void tearDown() throws Exception {
         super.tearDown();
     }
+
+    /*
+    Tabella Viaggio
+     */
 
     public void testInsertViaggio() throws Exception {
         String nomeViaggio = "nuovo viaggio";
@@ -56,12 +70,43 @@ public class MapperContentProviderTest extends ProviderTestCase2<MapperContentPr
         int index = cursor.getColumnIndex(MapperContract.Viaggio.NOME);
         String nome = cursor.getString(index);
         cursor.close();
-        assertEquals(nome, NOME);
+        assertEquals(nome, VIAGGIO);
     }
 
     public void testDeleteViaggio () throws Exception {
         Uri uri = ContentUris.withAppendedId(MapperContract.Viaggio.CONTENT_URI, 1);
         int rows = mResolver.delete(uri, null, null);
         assertEquals(rows, 1);
+    }
+
+    /*
+    Tabella Dati Citta
+     */
+
+    public void testNuoviDatiCitta() throws Exception {
+        String nome = "Milano";
+        String nazione = "Italia";
+        ContentValues values = new ContentValues();
+        values.put(MapperContract.DatiCitta.NOME, nome);
+        values.put(MapperContract.DatiCitta.NAZIONE, nazione);
+        values.put(MapperContract.DatiCitta.LATITUDINE, 0);
+        values.put(MapperContract.DatiCitta.LONGITUDINE, 0);
+        Uri uri = mResolver.insert(MapperContract.DatiCitta.CONTENT_URI, values);
+        assertNotNull(uri);
+        assertEquals(uri.getLastPathSegment(), "2");
+    }
+
+    /*
+    Tabella Citta
+     */
+
+    public void testCreaCitta () throws Exception {
+        ContentValues values = new ContentValues();
+        values.put(MapperContract.Citta.ID_VIAGGIO, 1);
+        values.put(MapperContract.Citta.ID_DATI_CITTA, 1);
+        values.put(MapperContract.Citta.PERCENTUALE, 0);
+        Uri uri = mResolver.insert(MapperContract.Citta.CONTENT_URI, values);
+        assertNotNull(uri);
+        assertEquals(uri.getLastPathSegment(), "1");
     }
 }
