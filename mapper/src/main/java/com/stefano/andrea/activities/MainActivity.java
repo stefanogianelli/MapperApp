d756527f430bf1472c90e045d900f7f373c7a591
andrea-mazzariello <andrea23.mazzariello@gmail.com>
2015-04-20 12:29:27 +0200

    FAB

--- mapper/src/main/java/com/stefano/andrea/activities/MainActivity.java
+++ mapper/src/main/java/com/stefano/andrea/activities/MainActivity.java
@@ -8,16 +8,21 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
 import com.stefano.andrea.adapters.ViaggiAdapter;
 import com.stefano.andrea.providers.MapperContract;
+import com.stefano.andrea.utils.FloatingActionButton;
 
 
-public class MainActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor> {
+public class MainActivity extends Activity implements LoaderManager.LoaderCallbacks<Cursor>, FloatingActionButton.OnCheckedChangeListener {
 
-
+    private final static String TAG = "FloatingActionButtonBasicFragment";
     private RecyclerView mRecyclerView;
     private ViaggiAdapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
@@ -42,8 +47,12 @@ public class MainActivity extends Activity implements LoaderManager.LoaderCallba
 
 
         // Floating button
+        FloatingActionButton fab1 = (FloatingActionButton) getWindow().getDecorView().findViewById(R.id.fab_1);
+        fab1.setOnCheckedChangeListener(this);
+ }
 
-    }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -81,5 +90,18 @@ public class MainActivity extends Activity implements LoaderManager.LoaderCallba
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
         mAdapter.swapCursor(null);
+    }
+
+
+    @Override
+    public void onCheckedChanged(FloatingActionButton fabView, boolean isChecked) {
+        // When a FAB is toggled, log the action.
+        switch (fabView.getId()){
+            case R.id.fab_1:
+                Log.d(TAG, String.format("FAB 1 was %s.", isChecked ? "checked" : "unchecked"));
+                break;
+            default:
+                break;
+        }
     }
 }
