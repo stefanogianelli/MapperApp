fb22e6b7bedab39af51087de04b0da1788be97ca
andrea-mazzariello <andrea23.mazzariello@gmail.com>
2015-04-21 18:18:57 +0200

    Varie

--- mapper/src/main/java/com/stefano/andrea/activities/DettagliViaggio.java
+++ mapper/src/main/java/com/stefano/andrea/activities/DettagliViaggio.java
@@ -4,18 +4,31 @@ package com.stefano.andrea.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.stefano.andrea.adapters.CittaAdapter;
 
 /**
  * Created by hp1 on 21-01-2015.
  */
 public class DettagliViaggio extends Fragment {
 
+    private RecyclerView mRecyclerView;
+    private CittaAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =inflater.inflate(R.layout.fragment_dettagli_viaggio,container,false);
+        mRecyclerView = (RecyclerView) v.findViewById(R.id.recycler_view_elenco_citta);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
         return v;
     }
 }
