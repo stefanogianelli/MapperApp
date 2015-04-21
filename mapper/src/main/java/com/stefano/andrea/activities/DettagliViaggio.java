9a0abd108606933fc51e23e4030ffba2e353ef7d
andrea-mazzariello <andrea23.mazzariello@gmail.com>
2015-04-21 18:39:49 +0200

    Add città

--- mapper/src/main/java/com/stefano/andrea/activities/DettagliViaggio.java
+++ mapper/src/main/java/com/stefano/andrea/activities/DettagliViaggio.java
@@ -1,6 +1,9 @@
 package com.stefano.andrea.activities;
 
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -9,6 +12,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.stefano.andrea.adapters.CittaAdapter;
 
@@ -30,5 +34,28 @@ public class DettagliViaggio extends Fragment {
         mRecyclerView.setLayoutManager(mLayoutManager);
         mRecyclerView.setAdapter(mAdapter);
         return v;
+    }
+
+    public void openDialogAddCitta(View view) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        builder.setView(inflater.inflate(R.layout.fragment_add_citta, null))
+                // Add action buttons
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Dialog d = (Dialog) dialog;
+                        EditText nn = (EditText) d.findViewById(R.id.text_add_citta_nn);
+                        EditText nc = (EditText) d.findViewById(R.id.text_add_citta);
+                        //mAdapter.creaNuovaCitta( nc.getText().toString() , nn.getText().toString())
+                        // TODO Finire add citta
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.dismiss();
+                    }
+                });
+        builder.create().show();
     }
 }
