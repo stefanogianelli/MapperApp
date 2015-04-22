969db59896c6588452e844edaa1ebfe62c47789b
andrea-mazzariello <andrea23.mazzariello@gmail.com>
2015-04-22 20:11:31 +0200

    Grid view per galleria immagini

--- mapper/src/main/java/com/stefano/andrea/activities/FotoViaggio.java
+++ mapper/src/main/java/com/stefano/andrea/activities/FotoViaggio.java
@@ -1,20 +1,51 @@
 package com.stefano.andrea.activities;
 
+import android.app.Activity;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+
+import com.stefano.andrea.adapters.GridViewFotoViaggioAdapter;
+import com.stefano.andrea.utils.ImageItem;
+
+import java.util.ArrayList;
 
 /**
  * Created by hp1 on 21-01-2015.
  */
 public class FotoViaggio extends Fragment {
+    private GridView gridView;
+    private GridViewFotoViaggioAdapter gridAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v =inflater.inflate(R.layout.fragment_foto_viaggio,container,false);
+
+        // Grid view per galleria immagini
+        gridView = (GridView) getActivity().findViewById(R.id.gridViewFotoViaggio);
+        gridAdapter = new GridViewFotoViaggioAdapter(getActivity(), R.layout.grid_item_foto_viaggio_layout, getData());
+        //gridView.setAdapter(gridAdapter);
+
         return v;
     }
+
+
+    // FZIONE TEMPORANEA...per cricare le varie immagini
+    private ArrayList<ImageItem> getData() {
+        final ArrayList<ImageItem> imageItems = new ArrayList<>();
+        TypedArray imgs = getResources().obtainTypedArray(R.array.image_ids);
+        for (int i = 0; i < imgs.length(); i++) {
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(), imgs.getResourceId(i, -1));
+            imageItems.add(new ImageItem(bitmap, "Image#" + i));
+        }
+        return imageItems;
+    }
+
 }
---
+++ mapper/src/main/java/com/stefano/andrea/utils/ImageItem.java
@@ -0,0 +1,33 @@
+package com.stefano.andrea.utils;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by Andre on 22/04/2015.
+ */
+public class ImageItem {
+    private Bitmap image;
+    private String title;
+
+    public ImageItem(Bitmap image, String title) {
+        super();
+        this.image = image;
+        this.title = title;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
