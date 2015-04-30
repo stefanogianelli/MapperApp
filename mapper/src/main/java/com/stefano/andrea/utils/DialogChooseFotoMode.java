75daa34a0aa747e8ef8e81e07205c4d2691466b3
andrea-mazzariello <andrea23.mazzariello@gmail.com>
2015-04-30 21:12:08 +0200

    Sistemato dialogo aggiunta foto

--- mapper/src/main/java/com/stefano/andrea/utils/DialogChooseFotoMode.java
+++ mapper/src/main/java/com/stefano/andrea/utils/DialogChooseFotoMode.java
@@ -1,13 +1,16 @@
 package com.stefano.andrea.utils;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+import android.app.Dialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+
+import com.stefano.andrea.activities.R;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -29,10 +32,31 @@ public class DialogChooseFotoMode {
      * @param imageUri L'uri dove si vuole salvare l'immagine (solo nel caso della fotocamera)
      */
     public static void mostraDialog (final Activity activity, final Uri imageUri) {
-        AlertDialog.Builder dialog = new AlertDialog.Builder(activity);
-        dialog.setTitle("Titolo");
-        dialog.setMessage("Scegli foto");
-        dialog.setPositiveButton("Gallery", new DialogInterface.OnClickListener() {
+        Dialog dialog = new Dialog(activity);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE); // Tolgo lo spazio per il titolo di default
+        dialog.setContentView(R.layout.fragment_add_foto);
+        dialog.findViewById(R.id.action_fotocamera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (imageUri != null) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+                    activity.startActivityForResult(intent, CAMERA_REQUEST);
+                } else {
+                    Log.v(TAG, "URI dell'immagine non settata!");
+                }
+            }
+        });
+        dialog.findViewById(R.id.action_galleria).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT, null);
+                intent.setType("image/*");
+                intent.putExtra("return-data", true);
+                activity.startActivityForResult(intent, GALLERY_PICTURE);
+            }
+        });
+        /*dialog.setPositiveButton("Gallery", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT, null);
@@ -52,7 +76,7 @@ public class DialogChooseFotoMode {
                     Log.v(TAG, "URI dell'immagine non settata!");
                 }
             }
-        });
+        });*/
         dialog.show();
     }
 
