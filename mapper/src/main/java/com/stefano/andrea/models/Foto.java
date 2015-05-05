4dde246f4833d6a2043f8407ef6a00a3a03a6a0f
stefanogianelli <stefano.gianelli@gmail.com>
2015-05-05 18:32:22 +0200

    Abilitata visione full foto dalle citta'

--- mapper/src/main/java/com/stefano/andrea/models/Foto.java
+++ mapper/src/main/java/com/stefano/andrea/models/Foto.java
@@ -1,21 +1,34 @@
 package com.stefano.andrea.models;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Foto
  */
-public class Foto {
+public class Foto implements Parcelable {
 
     private long id;
     private String path;
     private double latitudine;
     private double longitudine;
-    private double idViaggio;
-    private double idCitta;
-    private double idPosto;
+    private long idViaggio;
+    private long idCitta;
+    private long idPosto;
 
     public Foto () {
         this.id = -1;
         this.idPosto = -1;
+    }
+
+    public Foto (Parcel pc) {
+        id = pc.readLong();
+        path = pc.readString();
+        latitudine = pc.readDouble();
+        longitudine = pc.readDouble();
+        idViaggio = pc.readLong();
+        idCitta = pc.readLong();
+        idPosto = pc.readLong();
     }
 
     public long getId() {
@@ -50,27 +63,56 @@ public class Foto {
         this.longitudine = longitudine;
     }
 
-    public double getIdViaggio() {
+    public long getIdViaggio() {
         return idViaggio;
     }
 
-    public void setIdViaggio(double idViaggio) {
+    public void setIdViaggio(long idViaggio) {
         this.idViaggio = idViaggio;
     }
 
-    public double getIdCitta() {
+    public long getIdCitta() {
         return idCitta;
     }
 
-    public void setIdCitta(double idCitta) {
+    public void setIdCitta(long idCitta) {
         this.idCitta = idCitta;
     }
 
-    public double getIdPosto() {
+    public long getIdPosto() {
         return idPosto;
     }
 
-    public void setIdPosto(double idPosto) {
+    public void setIdPosto(long idPosto) {
         this.idPosto = idPosto;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeString(path);
+        dest.writeDouble(latitudine);
+        dest.writeDouble(longitudine);
+        dest.writeLong(idViaggio);
+        dest.writeLong(idCitta);
+        dest.writeLong(idPosto);
+    }
+
+    public static final Parcelable.Creator<Foto> CREATOR = new Parcelable.Creator<Foto>() {
+        @Override
+        public Foto createFromParcel(Parcel source) {
+            return new Foto(source);
+        }
+
+        @Override
+        public Foto[] newArray(int size) {
+            return new Foto[size];
+        }
+    };
+
 }
